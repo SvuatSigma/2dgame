e2b85f6d1cb2a7ad37f0f8cab8e0edd5425175ce
SvuatSigma <aleksbebra588@gmail.com>
2024-09-30 16:28:15 +0300

    add project

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,25 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     public float speed = 10f;
     private Rigidbody2D body;
-
+    private Animator anim;
+    public bool grounded;
     // Start is called before the first frame update
     void Start()
     {
-        body = GetComponent <Rigidbody2D>();
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
-        if (Input.GetKey(KeyCode.Space))
+        float horizontal = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontal * speed, body.velocity.y);
+        if (Input.GetKey(KeyCode.Space) && grounded == true)
+        {
+            Jump();
+        }
+        //flip
+        if (horizontal > -0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (horizontal < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+
+
+
+        //anim
+        anim.SetBool("move", horizontal != 0);
+
+    }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+
+        {
+            if (collision.gameObject.tag == "ground")
+            {
+                grounded = true;
+            }
+        }
+
+
+
+
+        private void Jump()
         {
             body.velocity = new Vector2(body.velocity.x, speed);
+            grounded = false;
         }
+
+
+
     }
-}
